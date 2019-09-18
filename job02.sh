ff20a9054932e20f8e9f7c96a22fba4624503766
Ginivieve Tangerine <gin.tan@monash.edu>
2019-09-19 08:24:11 +1000

    Changing nvidia command

--- job02.sh
+++ job02.sh
@@ -9,7 +9,8 @@
 #SBATCH --qos=desktopq
 
 # Start the nvidia-smi command
-nvidia-smi -l 1 -q -x -f nvidia-log &
+#nvidia-smi -l 1 -q -x -f nvidia-log &
+nvidia-smi dmon -s pucvt -o DT -d 10 -f nvidia-smi.dmon$1.$(date "+%Y.%m.%d-%H.%M.%S").csv &
 
 # Get its PID
 PID=$!
