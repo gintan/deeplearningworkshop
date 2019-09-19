842404233cef1671e9d5ee2381993de5751bb6ff
Ginivieve Tangerine <gin.tan@monash.edu>
2019-09-19 10:43:07 +1000

    Removing the QoS

--- job01.sh
+++ job01.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=firstjob
-#SBATCH --account=nq46
+#SBATCH --job-name=pytorch
+#SBATCH --account=training
 #SBATCH --time=01:00:00
-#SBATCH --ntasks=2
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:P4:2
-#SBATCH --partition=desktop
-#SBATCH --qos=desktopq
+#SBATCH --gres=gpu:P4:1
 
 module load pytorch
 python3 finetune_torchvision.py
